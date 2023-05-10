805370275dca8ec228ec1cd93c6ef22eeaf8cc50
KhoirunAriningAzizah <khoirun.20018@mhs.unesa.ac.id>
2023-05-10 08:36:24 +0700

    bismillah terakhir

--- resources/views/home_admin.blade.php
+++ resources/views/home_admin.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="col-lg-3 col-sm-6 col-xs-12">
             <div class="white-box analytics-info">
-                <h3 class="box-title">Jumlah Staf HR</h3>
+                <h3 class="box-title">Jumlah Pejabat Struktural</h3>
                 <ul class="list-inline two-part">
                     <li>
                     </li>
